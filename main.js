6401229fc11b15dd6927e7937c16285bf040edb3
lukeyzhong <lukeyzhong@gmail.com>
2023-10-10 15:36:20 -0500

    update

--- main.js
+++ main.js
@@ -1,49 +1,45 @@
 import * as THREE from 'three';
 
-let colors = new THREE.Color(0x00ff00);  // Use a THREE.Color instance for the initial color
-
-const changeColor = () => {
-    colors.set(0xFF0000);  // Change to another THREE.Color instance
-    cube.material.color.copy(colors);  // Update the material color
-}
-
-
-//----------------------
-
+// Create a scene
 const scene = new THREE.Scene();
 
-// four params, unit: degrees, ratio, near, far
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+// Create a camera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
 
+// Create a renderer
 const renderer = new THREE.WebGLRenderer();
-//height and width
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-// geometry: points and fill
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-//material
-const material = new THREE.MeshBasicMaterial( { color: colors } );
-// mesh helps to combine geo and mat and can move freely around
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+// Create a square geometry
+const geometry = new THREE.BufferGeometry();
+const vertices = new Float32Array( [
+	-1.0, -1.0,  1.0, // v0
+	 1.0, -1.0,  1.0, // v1
+	 1.0,  1.0,  1.0, // v2
+	-1.0,  1.0,  1.0, // v3
+] );
 
-//x represents the horizontal position (left to right).
-//y represents the vertical position (up and down).
-//z represents the depth or distance from the camera to the scene (front to back).
-camera.position.z = 3;
+const indices = [
+	0, 1, 2,
+	2, 3, 0,
+];
 
-// loop
-function animate() {
-    // requestAnimationFrame will destory the loop when out of the page
-	requestAnimationFrame( animate );
-    cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
-	renderer.render( scene, camera );
-}
-animate();
+geometry.setIndex( indices );
 
-//---------
-// Attach an event listener to the button
-const colorButton = document.getElementById('colorButton');
-colorButton.addEventListener('click', changeColor);
+geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
+
+// Create a material
+const material = new THREE.MeshBasicMaterial({ color: "pink" });
+
+// Create a mesh
+const mesh = new THREE.Mesh(geometry, material);
+
+// Add the mesh to the scene
+scene.add(mesh);
+
+// Render the scene once
+renderer.render(scene, camera);
+
+this.camera.fov 
